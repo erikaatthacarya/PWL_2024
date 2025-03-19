376e4395cea2c22b399dc2f25abfd56d7a85c470
erikaatthacarya <erikaputric48@gmail.com>
2025-03-19 12:41:42 +0700

    jobsheet 3-praktikum 2.1

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,44 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Halaman Home
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', [PageController::class,'index']);
-Route::get('/about', [PageController::class,'about']);
-Route::get('/articles/{id}', [PageController::class,'articles']);
-Route::resource('photos', PhotoController::class)->only([ 
-    'index', 'show' 
-    ]); 
-    Route::resource('photos', PhotoController::class)->except([ 
-    'create', 'store', 'update', 'destroy' 
-    ]); 
-    Route::get('/greeting', function () { 
-        return view('hello', ['name' => 'Erika']); 
-        });
-    Route::get('/greeting', [WelcomeController::class, 'greeting']);
-    Route::get('/', [HomeController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+
+
+
+
+
+// Halaman Products dengan Prefix Route
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
     Route::get('/home-care', [ProductController::class, 'homeCare']);
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
+
+// Halaman User dengan Route Parameter
 Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Halaman Penjualan (POS)
 Route::get('/sales', [SalesController::class, 'index']);
+
+
+?>
